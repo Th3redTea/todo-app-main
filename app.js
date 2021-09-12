25aa4e5ca97e240105091d15116b1bd956d2df5f
Amine <amhoume@gmail.com>
2021-09-12 13:04:23 +0100

    dark mode complete and todo is complete but it's not responssive

--- app.js
+++ app.js
@@ -1,12 +1,82 @@
 const input = document.getElementById('addTask')
 const addinput = document.querySelector('.addCheckBox')
 const list = document.getElementById('list')
-const items = document.getElementsByClassName('item')
 const clear = document.getElementById('clear')
+const itemsLeft = document.getElementById('itemsLeft')
 
-console.log(items)
+const unorederdList = document.querySelector('.list')
+const body = document.querySelector('body')
+const filtersContainer = document.querySelector('.filters-container')
+const item = document.querySelectorAll('.item')
+const lightPic = document.querySelector('.light-mode')
+const darkPic = document.querySelector('.dark-mode')
+
+const sunIcon = document.getElementById('sunIcon')
+const moonIcon = document.getElementById('moonIcon')
+
+
+sunIcon.addEventListener('click', () => {
+
+    lightPic.style.visibility = "visible"
+    darkPic.style.visibility = "hidden"
+
+
+    moonIcon.style.visibility = "visible"
+    sunIcon.style.visibility = "hidden"
+
+    input.style.backgroundColor = "white"
+    input.style.color = "hsl(235, 19%, 35%)"
+
+
+    unorederdList.style.backgroundColor = "white"
+
+    
+    body.style.backgroundColor = "white"
+    
+    filtersContainer.style.backgroundColor = "white"
+    filtersContainer.style.color = "hsl(236, 9%, 61%)" 
+    
+    if(item){
+        item.style.color = "hsl(236, 9%, 61%)" 
+    }
+
+})
+
+moonIcon.addEventListener('click', () => {
+
+    lightPic.style.visibility = "hidden"
+    darkPic.style.visibility = "visible"
+
+
+    moonIcon.style.visibility = "hidden"
+    sunIcon.style.visibility = "visible"
+
+    input.style.backgroundColor = "hsl(235, 24%, 19%)"
+    input.style.color = "hsl(234, 39%, 85%)"
+
+
+    const unorederdList = document.querySelector('.list')
+    unorederdList.style.backgroundColor = "hsl(235, 24%, 19%)"
+
+    
+    const body = document.querySelector('body')
+    body.style.backgroundColor = "hsl(235, 21%, 11%)"
+    
+    const filtersContainer = document.querySelector('.filters-container')
+    filtersContainer.style.backgroundColor = "hsl(235, 24%, 19%)"
+    filtersContainer.style.color = "hsl(234, 39%, 85%)" 
+    
+    const item = document.querySelectorAll('.item')
+    item.style.color = "hsl(236, 33%, 92%)" 
+})
+
+
+
+
 
 const todos = []
+const completedTodos = []
+const activeTodos = []
 
 const addTodo = (value) => {
     const item = {
@@ -45,19 +115,19 @@ const addTodo = (value) => {
         })
          
         
-        
     }
     
     
 }
 
 
-
 addinput.addEventListener('click', () => {
     addTodo(input.value)
+    itemsLeft.innerHTML = `${items.length} items left`
     input.value = ""     
 })
 
+const items = document.getElementsByClassName('item')
 
 
 
@@ -69,10 +139,29 @@ const clearComplete = (todo) => {
             item.remove()
  
             let idx = todos.indexOf(todo)
+
             if (idx !== -1) {
                     todos.splice(idx, 1)
             }
         }
+    })    
+
+}
+
+
+const showComplete = () => {
+    
+    Object.values(items).map(item => {
+        console.log(item)
+
+        if(item.classList.contains('complete')){
+            console.log('it has complete')
+            item.classList.remove('hidden')
+            item.classList.add('visible')
+        } else {
+            item.classList.remove('visible')
+            item.classList.add('hidden')
+        }   
     })    
 
 }
@@ -82,7 +171,48 @@ clear.addEventListener('click', () =>{
 
     todos.forEach(todo => {
         clearComplete(todo)
-        console.log(todos)
     });
 
 })
+
+
+const completed = document.getElementById('completed')
+
+completed.addEventListener('click', showComplete)
+
+
+const showActive = () => {
+    
+    
+    Object.values(items).map(item => {
+        if(item.classList.contains('complete')){
+            item.classList.remove('visible')
+            item.classList.add('hidden')
+            
+        } else {
+            item.classList.remove('hidden')
+            item.classList.add('visible')
+        }
+    })    
+
+}
+
+
+const showAll = () => {
+
+    Object.values(items).map(item => {
+            item.classList.remove('hidden')
+
+    })
+}    
+
+
+const active = document.getElementById('active')
+
+active.addEventListener('click', showActive)
+
+
+const all = document.getElementById('all');
+
+all.addEventListener('click', showAll)
+
--- dist/app.dev.js
+++ dist/app.dev.js
@@ -3,10 +3,52 @@
 var input = document.getElementById('addTask');
 var addinput = document.querySelector('.addCheckBox');
 var list = document.getElementById('list');
-var items = document.getElementsByClassName('item');
 var clear = document.getElementById('clear');
-console.log(items);
+var itemsLeft = document.getElementById('itemsLeft');
+var unorederdList = document.querySelector('.list');
+var body = document.querySelector('body');
+var filtersContainer = document.querySelector('.filters-container');
+var item = document.querySelectorAll('.item');
+var lightPic = document.querySelector('.light-mode');
+var darkPic = document.querySelector('.dark-mode');
+var sunIcon = document.getElementById('sunIcon');
+var moonIcon = document.getElementById('moonIcon');
+sunIcon.addEventListener('click', function () {
+  lightPic.style.visibility = "visible";
+  darkPic.style.visibility = "hidden";
+  moonIcon.style.visibility = "visible";
+  sunIcon.style.visibility = "hidden";
+  input.style.backgroundColor = "white";
+  input.style.color = "hsl(235, 19%, 35%)";
+  unorederdList.style.backgroundColor = "white";
+  body.style.backgroundColor = "white";
+  filtersContainer.style.backgroundColor = "white";
+  filtersContainer.style.color = "hsl(236, 9%, 61%)";
+
+  if (item) {
+    item.style.color = "hsl(236, 9%, 61%)";
+  }
+});
+moonIcon.addEventListener('click', function () {
+  lightPic.style.visibility = "hidden";
+  darkPic.style.visibility = "visible";
+  moonIcon.style.visibility = "hidden";
+  sunIcon.style.visibility = "visible";
+  input.style.backgroundColor = "hsl(235, 24%, 19%)";
+  input.style.color = "hsl(234, 39%, 85%)";
+  var unorederdList = document.querySelector('.list');
+  unorederdList.style.backgroundColor = "hsl(235, 24%, 19%)";
+  var body = document.querySelector('body');
+  body.style.backgroundColor = "hsl(235, 21%, 11%)";
+  var filtersContainer = document.querySelector('.filters-container');
+  filtersContainer.style.backgroundColor = "hsl(235, 24%, 19%)";
+  filtersContainer.style.color = "hsl(234, 39%, 85%)";
+  var item = document.querySelectorAll('.item');
+  item.style.color = "hsl(236, 33%, 92%)";
+});
 var todos = [];
+var completedTodos = [];
+var activeTodos = [];
 
 var addTodo = function addTodo(value) {
   var item = {
@@ -39,8 +81,10 @@ var addTodo = function addTodo(value) {
 
 addinput.addEventListener('click', function () {
   addTodo(input.value);
+  itemsLeft.innerHTML = "".concat(items.length, " items left");
   input.value = "";
 });
+var items = document.getElementsByClassName('item');
 
 var clearComplete = function clearComplete(todo) {
   Object.values(items).map(function (item) {
@@ -55,9 +99,48 @@ var clearComplete = function clearComplete(todo) {
   });
 };
 
+var showComplete = function showComplete() {
+  Object.values(items).map(function (item) {
+    console.log(item);
+
+    if (item.classList.contains('complete')) {
+      console.log('it has complete');
+      item.classList.remove('hidden');
+      item.classList.add('visible');
+    } else {
+      item.classList.remove('visible');
+      item.classList.add('hidden');
+    }
+  });
+};
+
 clear.addEventListener('click', function () {
   todos.forEach(function (todo) {
     clearComplete(todo);
-    console.log(todos);
   });
 });
+var completed = document.getElementById('completed');
+completed.addEventListener('click', showComplete);
+
+var showActive = function showActive() {
+  Object.values(items).map(function (item) {
+    if (item.classList.contains('complete')) {
+      item.classList.remove('visible');
+      item.classList.add('hidden');
+    } else {
+      item.classList.remove('hidden');
+      item.classList.add('visible');
+    }
+  });
+};
+
+var showAll = function showAll() {
+  Object.values(items).map(function (item) {
+    item.classList.remove('hidden');
+  });
+};
+
+var active = document.getElementById('active');
+active.addEventListener('click', showActive);
+var all = document.getElementById('all');
+all.addEventListener('click', showAll);
